59267c8f077ad4920fc672096a7f048727247448
Sahil Shrestha <sahiltuchhe123@gmail.com>
2024-12-29 05:12:25 -0500

    math sets

---
+++ week7/mathsets/set_Intersection.py
@@ -0,0 +1,11 @@
+#first  set
+A = {1,3,5}
+
+#second  set
+B = {0,2,4}
+
+#perform intersection operation using &
+print("Intersection using &:", A & B)
+
+#perform intersection operation using intersection()
+print("Intersection using intersection():", A.intersection(B))
---
+++ week7/set/set_iterate.py
@@ -0,0 +1,13 @@
+fruits = {"Apple","Peach","Mango"}
+
+#for loop to access each fruits
+for fruit in fruits:
+    print(fruits)
+print(len(fruits))
+
+
+#For length
+# even_number= {2,4,6,8}
+# print("Set:", even_number)
+
+# print("Total Elements", len(even_number))
